8c0f9510e624efc609a4262302e02f49c7840f16
Dmytro Bogovych <dmytro.bogovych@gmail.com>
2018-09-12 20:19:56 +0300

    - more changes to ease file handling

--- src/engine/helper/HL_File.cpp
+++ src/engine/helper/HL_File.cpp
@@ -1,10 +1,18 @@
 #include "HL_File.h"
 #include <fstream>
 
+#if defined(TARGET_LINUX) || defined(TARGET_OSX)
+# include <unistd.h>
+#endif
+
 bool FileHelper::exists(const std::string& s)
 {
+#if defined(TARGET_WIN)
     std::ifstream ifs(s);
     return !ifs.bad();
+#else
+    return (access(s.c_str(), R_OK) != -1);
+#endif
 }
 
 bool FileHelper::exists(const char* s)
@@ -28,4 +36,34 @@ std::string FileHelper::gettempname()
     tmpnam(buffer);
 
     return buffer;
+}
+
+bool FileHelper::isAbsolute(const std::string& s)
+{
+    if (s.empty())
+        return false;
+
+    return s.front() == '/' || s.front() == '\\';
+}
+
+std::string FileHelper::addTrailingSlash(const std::string& s)
+{
+    if (s.empty())
+        return "/";
+
+    if (s.back() == '/' || s.back() == '\\')
+        return s;
+
+    return s + "/";
+}
+
+std::string FileHelper::mergePathes(const std::string& s1, const std::string& s2)
+{
+    std::string result(addTrailingSlash(s1));
+    if (isAbsolute(s2))
+        result += s2.substr(1, s2.size() - 1);
+    else
+        result += s2;
+
+    return result;
 }
--- src/engine/helper/HL_File.h
+++ src/engine/helper/HL_File.h
@@ -13,6 +13,10 @@ public:
     static void remove(const char* s);
 
     static std::string gettempname();
+    static bool isAbsolute(const std::string& s);
+
+    static std::string addTrailingSlash(const std::string& s);
+    static std::string mergePathes(const std::string& s1, const std::string& s2);
 };
 
 #endif
