0eb5ad7cf7a39ebd30ad6f2196035efde82e3653
Dmytro Bogovych <dmytro.bogovych@gmail.com>
2023-05-14 12:04:02 +0300

    - add build script which targets Android platform

---
+++ build_android.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+rm -rf build_android
+mkdir -p build_android
+cd build_android
+
+cmake -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake      \
+      -DANDROID_NDK=$ANDROID_NDK_HOME                                                   \
+      -DANDROID_PLATFORM=24                                                             \
+      -DCMAKE_BUILD_TYPE=Release                                                        \
+      -DANDROID_ABI="armeabi-v7a"                                                       \
+      ../src
+
+cmake --build . -j8
+
