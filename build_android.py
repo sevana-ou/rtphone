d2e1b5e203452c8a7b01829190563ebae1a7454b
Dmytro Bogovych <dmytro.bogovych@gmail.com>
2026-02-01 19:37:23 +0300

    - switch to normal OpenSSL libraries

--- build_android.py
+++ build_android.py
@@ -7,11 +7,12 @@ import shutil
 # Temporary build directory
 DIR_BUILD = 'build_android'
 
-# Android NDK home directory 
+# Android NDK home directory
 NDK_HOME = os.environ['ANDROID_NDK_HOME']
-
+VCPKG_ROOT = os.environ['VCPKG_ROOT']
 # CMake toolchain file
 TOOLCHAIN_FILE = f'{NDK_HOME}/build/cmake/android.toolchain.cmake'
+TOOLCHAIN_FILE_VCPKG=f'{VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake'
 
 # This directory
 DIR_THIS = Path(__file__).parent.resolve()
@@ -23,9 +24,9 @@ def make_build() -> Path:
     if Path(DIR_BUILD).exists():
         shutil.rmtree(DIR_BUILD)
     os.mkdir(DIR_BUILD)
-    os.chdir(DIR_BUILD)  
-    
-    cmd = f'cmake -DCMAKE_TOOLCHAIN_FILE={TOOLCHAIN_FILE} '
+    os.chdir(DIR_BUILD)
+
+    cmd = f'cmake -DCMAKE_TOOLCHAIN_FILE={TOOLCHAIN_FILE} -DCMAKE_TOOLCHAIN_FILE={TOOLCHAIN_FILE_VCPKG} '
     cmd += f'-DANDROID_NDK=$NDK_HOME '
     cmd += f'-DANDROID_PLATFORM=24 '
     cmd += f'-DCMAKE_BUILD=Release '
@@ -34,11 +35,11 @@ def make_build() -> Path:
     retcode = os.system(cmd)
     if retcode != 0:
         raise RuntimeError('Problem when configuring the project')
-    
+
     cmd = f'cmake --build . -j {multiprocessing.cpu_count()}'
     retcode = os.system(cmd)
     if retcode != 0:
         raise RuntimeError('Problem when building the project')
-    
+
 if __name__ == '__main__':
     make_build()
